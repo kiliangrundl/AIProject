56df11ed928599c6dd36ba31b36889d3371a4c60
Kilian Grundl <kilian.grundl@gmail.com>
2017-01-10 23:47:34 +0100

    making it more normal with full fields as walls

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ from Tkinter import *
 import numpy
 from pip.utils.logging import colorama
 from numpy.matlib import rand
+import random
 
 class Fieldobject(object):
     
-    def __init__(self, position, color):
-        object.__init__(self)
-        self.position = numpy.array([float(position[0]),float(position[1])])
+    def __init__(self, color):
+        object.__init__(self)        
         self.color = color
+    
+    def setPosition(self, position):
+        self.position = numpy.array([float(position[0]),float(position[1])])
     
     def getPosition(self):
         return self.position
@@ -39,11 +42,12 @@ class Mover(Fieldobject):
     """
       Class for any Mover
     """
-    def __init__(self, position, color):
-        Fieldobject.__init__(self, position, color)
+    def __init__(self, color):
+        Fieldobject.__init__(self, color)
         self.lastDir = self.direction = numpy.array([0,0])
         self.score = 0
-        self.speed = 0.2
+        self.speed = 0.1
+        self.counter = 0
     
     def changeDir(self):
         if self.field.isValid(self.position, self.lastDir * self.speed):
@@ -54,7 +58,7 @@ class Mover(Fieldobject):
     
     def setDir(self, direction):
         self.lastDir = direction
-        self.counter = 3        
+        self.counter = int(1 / self.speed)
     
     def up(self):
         """
@@ -95,12 +99,13 @@ class Mover(Fieldobject):
     
 class Rag(Mover):
     
-    def __init__(self, position):
-        Mover.__init__(self, position, 'blue')
+    def __init__(self):
+        Mover.__init__(self, 'blue')
         self.score = 0
         
     def update(self):
-        self.changeDir()
+        if self.counter > 0:
+            self.changeDir()
         
     def getScore(self):
         return self.score
@@ -119,51 +124,100 @@ class Rag(Mover):
             self.down()            
        
 class Monster(Mover):
-    def __init__(self, position):
-        Mover.__init__(self, position, 'red')
+    def __init__(self):
+        Mover.__init__(self, 'red')
         
-    def update(self):
+    def update(self):        
         number = rand(1)
         
-        if number < 0.25:
-            self.up()
-        elif number < 0.5:
-            self.down()
-        elif number < 0.75:
-            self.left()
-        else:
-            self.right()
+        prob = 0.2 # probability to change direction
+        
+        if not self.field.isValid(self.position, self.direction) or number < prob:
+            #chose a new direction if the current one is not valid any more or with a certain probablity
+            number = rand(1)
+            if number < 0.25:
+                self.up()
+            elif number < 0.5:
+                self.down()
+            elif number < 0.75:
+                self.left()
+            else:
+                self.right()
             
+        # set new direction
         self.changeDir()
             
 class Coin(Fieldobject):
     
     def __init__(self, position):
-        Fieldobject.__init__(self, position, 'yellow')
+        Fieldobject.__init__(self, 'yellow')
+        self.setPosition(position)
      
     
 class Arena(Canvas):
 
-    def __init__(self, master, xfields, yfields):
+    def __init__(self, master, arenaNo):
         Canvas.__init__(self, master)
-        self.fields = numpy.array([xfields, yfields])
         self.fieldsize=50        
         self.walls = []
         self.monsters = []
-   
-    def neighbours(self, field1, field2):
-        return numpy.linalg.norm(numpy.subtract(field1, field2)) == 1
         
-    def addWall(self, field1, field2):
-        if self.neighbours(field1, field2):          
-            self.walls.append((numpy.array(field1), numpy.array(field2)))
+        self.initArena1()
+
+    def initArena1(self):
+                
+        self.fields = numpy.array([9, 9])
+        self.playerstart = numpy.array([0,0])
+        self.monsterStarts = []
+        self.monsterStarts.append(numpy.array([4,4]))
+        self.monsterStarts.append(numpy.array([4,5]))
+        
+        for i in range(2):
+            self.addMonster(Monster())
+        
+        #AROUND the monster Start
+        for x in range(3,6):
+            y = 3
+            self.addWall((x,y))
+            
+        for y in range(4,6):
+            for x in [3,5]:
+                self.addWall((x,y))
+        
+        #OUTSIDE    
+        for x in range(1,4):
+            for y in [1,7]:
+                self.addWall((x,y))
+            
+        for x in range(5,8):
+            for y in [1,7]:
+                self.addWall((x,y))
+                
+        for y in range(1,4):
+            for x in [1,7]:
+                self.addWall((x,y))
+                
+        for y in range(5,8):
+            for x in [1,7]:
+                self.addWall((x,y))
+                
+                    
+   
+    def addWall(self, field):
+        field = numpy.array(field)
+        if not any((field == x).all() for x in self.walls):
+            self.walls.append(field)
         
     def getField(self, position):
         return position.round()
         
     def isValid(self, position, direction):
         field = self.getField(position)
-        newField = self.getField((position) + direction/numpy.linalg.norm(direction)*0.51)
+        nrm = numpy.linalg.norm(direction)
+        if nrm > 1e-6:
+            newField = self.getField((position) + direction/nrm*0.51)
+        else:
+            newField = field
         if (newField < 0).any() or (newField >= self.fields).any():
             return False
         
@@ -171,19 +225,16 @@ class Arena(Canvas):
         
         if numpy.linalg.norm(validDir) < 1e-6:
             # Is in the center of the field
-            for wall in self.walls:
-                if ((field == wall[0]).all() and (newField == wall[1]).all())or \
-                   ((field == wall[1]).all() and (newField == wall[0]).all()):
-                    return False
+            if any((newField == x).all() for x in self.walls):
+                return False
         else:
             if numpy.abs(numpy.dot(validDir, direction)) < 1e-6:
                 return False
         return True
-            
         
-        
-    def addPlayer(self, pacman):
-        self.rag = pacman
+    def addPlayer(self, rag):
+        self.rag = rag
+        self.rag.setPosition(self.playerstart)
         
     def addMonster(self, ghost):
         self.monsters.append(ghost)                    
@@ -192,31 +243,37 @@ class Arena(Canvas):
         self.coins = []
         for x in range(0,self.fields[0]):
             for y in range(0,self.fields[1]):
-                self.coins.append(Coin((x,y)))
+                newField = numpy.array([x,y])
+                if not any((newField == x).all() for x in self.walls) and \
+                    not any((newField == x).all() for x in self.monsterStarts):
+                    self.coins.append(Coin((x,y)))
                  
-        self.rag.setBattlefield(self)            
-        for ghost in self.monsters:
-            ghost.setBattlefield(self)
+        self.rag.setBattlefield(self)         
+        for monster in self.monsters:
+            monster.setBattlefield(self)
+            monster.setPosition(random.choice(self.monsterStarts))
+            
         
         # DRAWING
-        self.battlefield = self.create_rectangle(0,0,self.fields[0]*self.fieldsize,self.fields[1] * self.fieldsize,fill='green')     
+        self.battlefield = self.create_rectangle(0,0,self.fields[0]*self.fieldsize,self.fields[1] * self.fieldsize,fill='green')
+        
+        for mS in self.monsterStarts:
+            xstart = mS[0] * self.fieldsize
+            ystart = mS[1] * self.fieldsize
+            self.create_rectangle(xstart,ystart,xstart + self.fieldsize,ystart + self.fieldsize,fill='red')
+        
         for wall in self.walls:
-            if wall[0][0] != wall[1][0]:
-                """
-                Same x-value
-                """
-                xmin = (1+wall[0][0])*self.fieldsize
-                xmax = xmin                
-                ymin = (wall[0][1]) *self.fieldsize
-                ymax = ymin + self.fieldsize                
-            else:
-                xmin = (wall[0][0])*self.fieldsize
-                xmax = xmin + self.fieldsize
-                
-                ymin = (1+wall[0][1]) *self.fieldsize
-                ymax = ymin
-            self.create_line(xmin, ymin, xmax, ymax, fill='red', width=2)
-            
+            xstart = wall[0] * self.fieldsize
+            ystart = wall[1] * self.fieldsize
+            self.create_rectangle(xstart,ystart,xstart + self.fieldsize,ystart + self.fieldsize,fill='gray')
+
+
+        for coin in self.coins:
+            x = (coin.getPosition()[0]+0.5)*self.fieldsize
+            y = (coin.getPosition()[1]+0.5)*self.fieldsize
+            r = self.fieldsize*0.1
+            w = self.create_oval(x-r, y-r, x+r, y+r, fill=coin.getColor())
+            coin.setTkObject(w)            
             
             
         x = (self.rag.getPosition()[0]+0.5)*self.fieldsize
@@ -224,23 +281,17 @@ class Arena(Canvas):
         r = self.fieldsize*0.4
         w = self.create_oval(x-r, y-r, x+r, y+r, fill=self.rag.getColor())
         self.rag.setTkObject(w)
-            
-        for ghost in self.monsters:
-            x = (ghost.getPosition()[0]+0.5)*self.fieldsize
-            y = (ghost.getPosition()[1]+0.5)*self.fieldsize
-            r = self.fieldsize*0.4
-            w = self.create_oval(x-r, y-r, x+r, y+r, fill=ghost.getColor())
-            ghost.setTkObject(w)
-
-        self.scorefield = self.create_text(self.fields[0]*self.fieldsize + 10, 20, text=str(self.rag.getScore()))
-            
-        for coin in self.coins:
-            x = (coin.getPosition()[0]+0.5)*self.fieldsize
-            y = (coin.getPosition()[1]+0.5)*self.fieldsize
-            r = self.fieldsize*0.1
-            w = self.create_oval(x-r, y-r, x+r, y+r, fill=coin.getColor())
-            coin.setTkObject(w)
         
+        self.scorefield = self.create_text(self.fields[0]*self.fieldsize + 30, 20, text=str(self.rag.getScore()))
+            
+        for monster in self.monsters:           
+            x = (monster.getPosition()[0]+0.5)*self.fieldsize
+            y = (monster.getPosition()[1]+0.5)*self.fieldsize
+            r = self.fieldsize*0.4
+            w = self.create_oval(x-r, y-r, x+r, y+r, fill=monster.getColor())
+            monster.setTkObject(w)
+        
+        self.master.geometry('{}x{}'.format(self.fields[0]*self.fieldsize, self.fields[1] * self.fieldsize))
         
     def refresh(self):
         """
@@ -280,43 +331,36 @@ class Arena(Canvas):
             
         #Drawing
         self.delete(self.scorefield)
-        self.scorefield = self.create_text(self.fields[0]*self.fieldsize + 10, 20, text=str(self.rag.getScore()))
+        self.scorefield = self.create_text(self.fields[0]*self.fieldsize + 30, 20, text=str(self.rag.getScore()))
         
         lost = False
         for monster in self.monsters:
             if (self.getField(self.rag.getPosition()) == self.getField(monster.getPosition())).all():
                 lost = True
+                self.rag.addScore(-500)
+                break
         
         if self.coins == []:
-            self.create_text(self.fields[0]*self.fieldsize / 2, self.fields[1]*self.fieldsize / 2 , text='You Won')
+            self.rag.addScore(500)
+            self.create_text(self.fields[0]*self.fieldsize / 2, self.fields[1]*self.fieldsize / 2 , text='You Won, Score = ' + str(self.rag.getScore()), fill='white')
         elif lost:
-            self.create_text(self.fields[0]*self.fieldsize / 2, self.fields[1]*self.fieldsize / 2 , text='You Lost')
+            self.create_text(self.fields[0]*self.fieldsize / 2, self.fields[1]*self.fieldsize / 2 , text='You Lost, Score = ' + str(self.rag.getScore()), fill='white')
         else:
             self.after(10, self.refresh)
    
 master = Tk()
 
-w = Arena(master, 5,5)
-for x in [1,2,3]:
-    for y in [0,3]:
-        w.addWall((x,y), (x,y+1))
-        
-for x in [0,3]:
-    for y in [1, 3]:
-        w.addWall((x,y), (x+1,y))
+w = Arena(master, 1)
+  
 
-w.addWall((2,1), (2,2))        
-w.addWall((2,2), (2,3))  
-
-rag = Rag((2,1))
+rag = Rag()
 w.addPlayer(rag)
-w.addMonster(Monster((0,0)))
-
 master.bind("<Key>", rag.keyInput)
-      
-w.pack()
 
 w.initialize()
+w.pack(fill=BOTH, expand=1)
+master.resizable(width=False, height=False)
+
 master.after(1000, w.refresh)
 mainloop()    
 
